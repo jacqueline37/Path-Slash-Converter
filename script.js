112b3cc6c70627925bd9e8e1dcbc08dafdbd8337
Jacqueline <hektar.nolmyra36@gmail.com>
2026-03-18 21:41:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,8 +21,13 @@ function countMatches(text, regex) {
   return (text.match(regex) || []).length;
 }
 
+function normalizeYenToBackslash(text) {
+  return text.replace(/¥/g, "\\");
+}
+
 function swapSlashes() {
-  const source = input.value;
+  const source = normalizeYenToBackslash(input.value);
+
   const slashCount = countMatches(source, /\//g);
   const backslashCount = countMatches(source, /\\/g);
   const totalCount = slashCount + backslashCount;
@@ -34,7 +39,7 @@ function swapSlashes() {
     .replace(/\//g, "\\")
     .replace(new RegExp(tempToken, "g"), "/");
 
-  setStatus("${totalCount}個の / と \\（¥）を変換しました");
+  setStatus(`${totalCount}個の / と \\（¥）を変換しました`);
 }
 
 async function copyOutput() {
